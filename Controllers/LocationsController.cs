4dcd38dadf0230ebd523544507a6c7688cbccce9
Camilo López Castro <camilopezc@hotmail.com>
2024-02-09 10:40:22 -0500

    Add Controller with Get and Post actions. Add Swagger UI options for d

---
+++ Controllers/LocationsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LocationsAvailability.Infrastructure;
+using LocationsAvailability.Models;
+
+namespace LocationsAvailability.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LocationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Locations
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Location>>> GetLocations()
+        {
+            return await _context.Locations.ToListAsync();
+        }
+
+        // GET: api/Locations/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Location>> GetLocation(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+
+            if (location == null)
+            {
+                return NotFound();
+            }
+
+            return location;
+        }
+
+        // POST: api/Locations
+        [HttpPost]
+        public async Task<ActionResult<Location>> PostLocation(Location location)
+        {
+            _context.Locations.Add(location);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetLocation", new { id = location.Id }, location);
+        }
+    }
+}
